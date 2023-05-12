e6db5615e56aac9bcb65b78fd1269438503fca00
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-13 01:48:17 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Button } from 'components/Button/Button';
+import { SearchBar } from './SearchBar/SearchBar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { getSearchImage } from './api/getSearchImage';
+
+export class App extends Component {
+  state = {
+    q: '',
+    pictures: null,
+    page: 1,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.page !== this.state.page) {
+      getSearchImage(this.state.q, this.state.page).then(({ hits }) => {
+        this.setState({
+          pictures: hits,
+        });
+      });
+    }
+  }
+
+  nextPage = () => {
+    this.setState({
+      page: this.state.page + 1,
+    });
+  };
+
+  handleFormSubmit = q => {
+    this.setState({
+      q,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSubmit={this.handleFormSubmit} />
+        <ImageGallery
+          images={this.state.pictures}
+          searhQuerry={this.state.q}
+          page={this.state.page}
+        />
+        <Button onClick={this.nextPage} />
+      </div>
+    );
+  }
+}
