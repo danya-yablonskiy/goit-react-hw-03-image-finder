56601b3089d40d79d0584290208822f7043213b3
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-14 00:55:03 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,7 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 export function ImageGallery({ images, showModal }) {
   return (
+    
     <ul className="ImageGallery">
       {images.map(image => (
         <ImageGalleryItem key={image.id} image={image} showModal={showModal} />
