10efb0fda79f765df3f46b4bbbfec34425fafc0a
danya-yablonskiy <danya.yablonskiy.00@gmail.com>
2023-05-13 23:31:12 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,38 +1,10 @@
-import { Component } from 'react';
-import { getSearchImage } from '../api/getSearchImage';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-
-export class ImageGallery extends Component {
-  state = {
-    images: null,
-  };
-
-  componentDidUpdate(prevProps, _) {
-    if (prevProps.searhQuerry !== this.props.searhQuerry) {
-      getSearchImage(this.props.searhQuerry, this.props.page).then(
-        ({ hits }) => {
-          const images = this.props.images;
-          console.log(images);
-          if (images) {
-            this.setState({
-              images: [...hits, ...images],
-            });
-          }
-          this.setState({
-            images: [...hits],
-          });
-        }
-      );
-    }
-  }
-
-  render() {
-    return (
-      <>
-        <ul className="gallery">
-          <ImageGalleryItem images={this.state.images} />
-        </ul>
-      </>
-    );
-  }
+export function ImageGallery({ images, showModal }) {
+  return (
+    <ul className="ImageGallery">
+      {images.map(image => (
+        <ImageGalleryItem key={image.id} image={image} showModal={showModal} />
+      ))}
+    </ul>
+  );
 }
